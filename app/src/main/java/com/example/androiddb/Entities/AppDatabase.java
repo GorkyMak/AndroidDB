9dd8dbebc481b2ad761f19844b6dbaf5b9c84caa
GorkyMak <m.kalinaev@gmail.com>
2021-03-02 18:33:37 +0300

    1 Отключение экспорта "схемы"

--- app/src/main/java/com/example/androiddb/Entities/AppDatabase.java
+++ app/src/main/java/com/example/androiddb/Entities/AppDatabase.java
@@ -6,7 +6,7 @@ import androidx.room.RoomDatabase;
 import com.example.androiddb.Entities.Users.Users;
 import com.example.androiddb.Entities.Users.UsersDao;
 
-@Database(entities = {Users.class}, version = 1)
+@Database(entities = {Users.class}, version = 1, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract UsersDao usersDao();
 }
